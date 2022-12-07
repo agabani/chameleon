fbed93ac861843b4a96b7bdba1d326396db7322f
agabani <agabani@users.noreply.github.com>
2022-12-07 22:37:15 +0000

    feat: topic card component

--- crates/chameleon-frontend/src/lib.rs
+++ crates/chameleon-frontend/src/lib.rs
@@ -7,14 +7,41 @@ use std::rc::Rc;
 
 use yew::prelude::*;
 
-use crate::{components::chat::Chat, services::Service};
+use crate::{
+    components::{chat::Chat, topic_card::TopicCard},
+    services::Service,
+};
 
 #[function_component]
 pub fn App() -> Html {
     let service = use_memo(|_| Service::default(), ());
 
+    let name = "Sports".to_string();
+    let secret_words = vec![
+        "Football",
+        "Basketball",
+        "Tennis",
+        "Lacrosse",
+        "Soccer",
+        "Ice Hockey",
+        "Badminton",
+        "Volleyball",
+        "Golf",
+        "Sailing",
+        "Motor Racing",
+        "Triathlon",
+        "Baseball",
+        "Squash",
+        "Wrestling",
+        "Cycling",
+    ]
+    .into_iter()
+    .map(AttrValue::from)
+    .collect::<Vec<_>>();
+
     html! {
         <ContextProvider<Rc<Service>> context={service}>
+            <TopicCard {name} {secret_words} />
             <Chat />
         </ContextProvider<Rc<Service>>>
     }
