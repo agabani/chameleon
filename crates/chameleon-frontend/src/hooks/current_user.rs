dac439d46684a1e172b35f119506be19c1ec42e3
agabani <agabani@users.noreply.github.com>
2022-12-30 15:51:28 +0000

    refactor: use current user hook

--- crates/chameleon-frontend/src/hooks/current_user.rs
+++ crates/chameleon-frontend/src/hooks/current_user.rs
@@ -7,51 +7,36 @@ use yew::{
 
 use crate::contexts::network::NetworkContext;
 
-#[derive(Default, PartialEq)]
-pub struct CurrentUserState {
-    response: Option<ResourcesDocument<UserAttributes>>,
-}
-
 #[hook]
 pub fn use_current_user() -> SuspensionResult<ResourcesDocument<UserAttributes>> {
     let network = use_context::<NetworkContext>().unwrap();
-    let state = use_reducer(CurrentUserState::default);
+    let state = use_state(|| None::<ResourcesDocument<UserAttributes>>);
 
-    if let Some(document) = &state.response {
-        return Ok(document.clone());
+    if let Some(state) = state.as_ref() {
+        return Ok(state.clone());
     }
 
     let (suspension, handle) = Suspension::new();
 
     spawn_local(async move {
         let Ok(userinfo) = network.get_userinfo().await else {
-            state.dispatch(CurrentUserState {
-                response: Some(ResourcesDocument::internal_server_error())
-            });
+            state.set(Some(ResourcesDocument::internal_server_error()));
             return handle.resume();
         };
 
         let Some(userinfo) = userinfo else {
-            state.dispatch(CurrentUserState {
-                response: Some(ResourcesDocument::unauthorized())
-            });
+            state.set(Some(ResourcesDocument::unauthorized()));
             return handle.resume();
         };
 
-        let user = network.get_user(&userinfo.sub).await.unwrap();
-        state.dispatch(CurrentUserState {
-            response: Some(user),
-        });
+        let Ok(user) = network.get_user(&userinfo.sub).await else {
+            state.set(Some(ResourcesDocument::internal_server_error()));
+            return handle.resume();
+        };
+
+        state.set(Some(user));
         handle.resume();
     });
 
     Err(suspension)
-}
-
-impl Reducible for CurrentUserState {
-    type Action = Self;
-
-    fn reduce(self: std::rc::Rc<Self>, action: Self::Action) -> std::rc::Rc<Self> {
-        action.into()
-    }
 }
